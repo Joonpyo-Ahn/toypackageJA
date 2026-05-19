d5dddd53a041f0a840e7c8ce9f080f2fd5670d86
Joonpyo-Ahn <ahnjo@lawrence.edu>
2026-05-19 16:40:32 +0000

    added assertion

--- R/min_max_scale.R
+++ R/min_max_scale.R
@@ -8,8 +8,11 @@
 #' @examples
 #' x <- 1:5
 #' min_max_scale(x)
+#'
 min_max_scale <- function(x)
 {
+  checkmate::assert_numeric(x)
+
   y <- (x - min(x,na.rm = TRUE))/(max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
 
   return(y)
